d01352680cb4c29a3c912b4e23ca390a664e1d46
Rad <jaaroos@gmail.com>
2025-04-08 15:00:21 +0200

    Apr

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -5,7 +5,6 @@ import { Separator } from "./ui/separator";
 type Props = {
   daily: number;
   liquidity: number;
-  apr: number;
   projects: number;
 };
 
@@ -16,23 +15,25 @@ type CardProps = {
   icon: string;
 };
 
-export default function Summary({ daily, liquidity, apr, projects }: Props) {
-  const Card = ({ title, numbers, subtitle, icon }: CardProps) => (
-    <div className="flex">
-      <Image
-        className="m-2"
-        height={64}
-        width={64}
-        src={`/${icon}`}
-        alt={title}
-      />
-      <div className="flex flex-col justify-center">
-        <span className="text-spi-green">{title}</span>
-        <b className="text-xl">{numbers}</b>
-        <span>{subtitle || <wbr />}</span>
-      </div>
+const Card = ({ title, numbers, subtitle, icon }: CardProps) => (
+  <div className="flex">
+    <Image
+      className="m-2"
+      height={64}
+      width={64}
+      src={`/${icon}`}
+      alt={title}
+    />
+    <div className="flex flex-col justify-center">
+      <span className="text-spi-green">{title}</span>
+      <b className="text-xl">{numbers}</b>
+      <span>{subtitle || <wbr />}</span>
     </div>
-  );
+  </div>
+);
+
+export default function Summary({ daily, liquidity, projects }: Props) {
+  const apr = (daily * 365 * 100) / liquidity;
   return (
     <div className="flex my-3">
       <Card
@@ -45,7 +46,7 @@ export default function Summary({ daily, liquidity, apr, projects }: Props) {
         title="Liquidity provided"
         subtitle={
           <>
-            <b className="text-spi-dark-green">{apr}%</b> APR
+            <b className="text-spi-dark-green">{Math.round(apr)}%</b> APR
           </>
         }
         numbers={`$${Math.round(liquidity).toLocaleString()}`}
