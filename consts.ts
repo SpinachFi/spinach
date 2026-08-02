133b557e0f90a0d59244b25079dffc0b1cb052d3
Jen <60794961+jennyg0@users.noreply.github.com>
2026-08-01 22:14:51 -0400

    august update (#121)

--- consts.ts
+++ consts.ts
@@ -52,12 +52,12 @@ export const AVAILABLE_CHAINS: ChainName[] = Object.keys(
 export const DEFAULT_CHAIN = AVAILABLE_CHAINS[0];
 
 export const ACTIVE_CAMPAIGNS = {
-  CELO_USDGLO: "usdglo14",
+  CELO_USDGLO: "usdglo15",
   CELO_REGEN: "regen4",
   CELO_GOODDOLLAR: "gooddollar2",
   ARBITRUM: "arbitrum",
-  STELLAR: "stellar9",
-  SUPERCHAIN: "superchain8",
+  STELLAR: "stellar10",
+  SUPERCHAIN: "superchain9",
 } as const;
 
 export const TALLY = {
